4a2a64c673ef17e706717083a74e53fc239f607c
itsyoru <sstaminup97@gmail.com>
2025-02-09 18:36:27 -0500

    home page ui

--- notes/ContentView.swift
+++ notes/ContentView.swift
@@ -9,13 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Text("😔")
+                    .font(.system(size: 45))
+                Text("No Notes Found!")
+                    .font(.system(size: 28, design: .rounded))
+            }
+            .navigationTitle("Notes App")
+            .toolbar {
+                ToolbarItem(placement: .bottomBar) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 50)
+                            .symbolRenderingMode(.hierarchical)
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
