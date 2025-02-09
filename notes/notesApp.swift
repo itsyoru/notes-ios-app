bdc0f1db69be991110d93121c916dbb3feba1ce2
itsyoru <sstaminup97@gmail.com>
2025-02-09 18:01:08 -0500

    Initial Commit

---
+++ notes/notesApp.swift
@@ -0,0 +1,17 @@
+//
+//  notesApp.swift
+//  notes
+//
+//  Created by Fawad Khan on 2/9/25.
+//
+
+import SwiftUI
+
+@main
+struct notesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
